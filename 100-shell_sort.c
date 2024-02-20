17a80466ed189ab85d504fae70cec1f998d37a62
Z24B <zivha.beharie@gmail.com>
2024-02-20 03:25:22 -0800

    Task 4

---
+++ 100-shell_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * shell_sort -  sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t interval = 1;
+	size_t i, j;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (interval < size / 3)
+		interval =  interval * 3 + 1;
+
+	while (interval >= 1)
+	{
+		for (i = interval; i < size; i++)
+		{
+			temp = array[1];
+			j = i;
+
+			while (j >= interval && array[j - interval] > temp)
+			{
+				array[j] = array[j - interval];
+				j -= interval;
+			}
+
+			array[j] = temp;
+		}
+		print_array(array, size);
+		interval /= 3;
+	}
+}
